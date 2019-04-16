87f7b8f2fc9dbf107223bb5bde64fe529f0bd054
EISMAN CAMARA ABEL <abel_c@live.fr>
2019-04-16 15:21:16 +0200

    Formulaire de importation

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
     <div class="container">
         <div class="row ">
             <!-- formulaire  -->
+			<form method="POST" action="#">
+            <button><a href="ImportBase.html">importation de base</a></button>
+            <button><a href="FaitMain.html">Ajouter à la main</a></button>           
+            </form>
         </div>
         <div class="row ">
             <!-- Affichage groupe  -->
