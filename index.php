ea893b476b806c08e16763e5bacf5e7a74da8707
bluetsuki <bluetsuki2001@gmail.com>
2019-04-16 14:17:16 +0200

    template base

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Slip Up</title>
+    <meta charset="uft-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+    </div>
+</body>
+</html>
